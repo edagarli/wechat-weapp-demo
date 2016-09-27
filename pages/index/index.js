9e5fc22a19dc9d43b74de8924aedb8454c094f2e
edagarli <lizhi@lydiabox.com>
2016-09-27 14:34:10 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,40 +4,24 @@ var app = getApp()
 Page({
   data: {
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      'http://www.zm1717.com/images/index/bd_nei_1.jpg',
+      'http://www.zm1717.com/images/index/bd_nei_2.jpg',
+      'http://www.zm1717.com/images/index/bd_nei_3.jpg'
     ],
     indicatorDots: true,
     autoplay: true,
-    interval: 3000,
+    interval: 2000,
     duration: 1000,
     navItems:[
       {
-        name:'堂食',
-        url:'dishes'
-      },
-      {
-        name:'外卖',
+        name:'吃饭视频直播',
         url:'take',
         isSplot:true
       },
       {
-        name:'外带',
-        url:'out'
-      },
-      {
-        name:'订单',
-        url:'bill'
-      },
-      {
-        name:'帐单',
-        url:'bill',
-        isSplot:true
-      },
-      {
-        name:'报表',
-        url:'bill'
+        name:'点餐',
+        url:'dishes',
+         isSplot:true
       }
     ]
   },
